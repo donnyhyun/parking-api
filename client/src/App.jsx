1957085ebc36500e65c66a04b4b830ea4ef5c7da
Don Lee <donnyhyun288@gmail.com>
2024-05-16 13:27:37 +0900

    added keypad component

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import TicketPage from './components/TicketPage'
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Parking Service </h1>
+      <TicketPage />
+    </div>
+  );
+}
+
+export default App;
