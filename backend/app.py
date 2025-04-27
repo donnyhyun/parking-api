96948861370700c10fb0af526c4932f51f29bc04
Don Lee <donnyhyun288@gmail.com>
2025-04-27 21:51:21 +0900

    call backend api

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,6 @@
 import os
 from flask import Flask
+from flask_cors import CORS
 from models.models import db
 from init import populate_db
 from parkingLot import park_app
@@ -9,6 +10,7 @@ from parkingStack import lot_app
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
+CORS(app)
 app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///' + os.path.join(basedir, 'db/parkinglot.db')
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.json.sort_keys = False
@@ -28,4 +30,4 @@ if __name__ == '__main__':
         db.drop_all()
         db.create_all()
         populate_db()
-    app.run(debug=True)
+    app.run(port=5001, debug=True)
--- client/src/components/TicketPage.jsx
+++ client/src/components/TicketPage.jsx
@@ -4,16 +4,42 @@ import Button from '@mui/material/Button'
 import styled from 'styled-components';
 import KeyPad from './KeyPad'
 
+const TicketTextField = styled(TextField)`
+  margin-top: 6px;
+`;
+
 function TicketPage() {
-  const [input, setInput] = useState('');
+  const [licensePlate, setLicensePlate] = useState('');
+  const [vehicleName, setVehicleName] = useState('');
 
   const handleButtonClick = (value) => {
     if (value === 'C') {
-      setInput('');
+      setLicensePlate('');
     } else if (value === '←') {
-      setInput(prevInput => prevInput.slice(0, -1));
+      setLicensePlate(prevInput => prevInput.slice(0, -1));
     } else {
-      setInput(prevInput => prevInput + value);
+      setLicensePlate(prevInput => prevInput + value);
+    }
+  };
+
+  const handleEnterClick = async () => {
+    try {
+      const response = await fetch(`http://127.0.0.1:5001/park?lot_id=1`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          name: vehicleName,
+          plate: licensePlate,
+          size: 'medium',
+        }),
+      });
+
+      const data = await response.json();
+      console.log('Server response:', data);
+    } catch (error) {
+      console.error('Error sending license plate:', error);
     }
   };
 
@@ -21,17 +47,27 @@ function TicketPage() {
     <div>
       <h1>Parking Service </h1>
       <div>
+        <label>Enter your vehicle model: </label>
+      </div>
+      <TicketTextField
+        id="vehicle-name"
+        label="Vehicle Name"
+        value={vehicleName}
+        onChange={(e) => setVehicleName(e.target.value)}
+        variant="outlined"
+      />
+      <div>
         <label>Enter your license plate: </label>
       </div>
-      <TextField
+      <TicketTextField
         id="outlined-basic"
         label="License #"
-        value={input}
-        onChange={(e) => setInput(e.target.value)}
+        value={licensePlate}
+        onChange={(e) => setLicensePlate(e.target.value)}
         variant="outlined"
       />
       <KeyPad onButtonClick={handleButtonClick} />
-      <Button variant="contained" >Enter</Button>
+      <Button variant="contained" onClick={handleEnterClick}>Enter</Button>
     </div>
   )
 }
